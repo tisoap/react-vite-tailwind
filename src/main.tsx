2cd61c6b423d546514aad74b896ab4c256fcb2ed
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2025-08-07 18:01:53 -0300

    Add TanStack Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-);
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+	throw new Error("Failed to find the root element");
+}
+
+if (!rootElement.innerHTML) {
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	);
+}
