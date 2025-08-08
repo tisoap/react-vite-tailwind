daaef3dd4bf5701b8f1a29608bc9a6919b05e0f4
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2025-08-08 09:41:19 -0300

    TanStack router dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,17 +1,21 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { Header } from "@components/Header";
 import { Footer } from "@components/Footer";
 
 export const Route = createRootRoute({
 	component: () => (
-		<div className="min-h-screen bg-gray-100 text-slate-800 flex flex-col">
-			<Header />
+		<>
+			<div className="min-h-screen bg-gray-100 text-slate-800 flex flex-col">
+				<Header />
 
-			<main className="flex-1 flex items-center justify-center">
-				<Outlet />
-			</main>
+				<main className="flex-1 flex items-center justify-center">
+					<Outlet />
+				</main>
 
-			<Footer>Copyright © 2025 Tiso Alvarez Puccinelli</Footer>
-		</div>
+				<Footer>Copyright © 2025 Tiso Alvarez Puccinelli</Footer>
+			</div>
+			<TanStackRouterDevtools />
+		</>
 	),
 });
